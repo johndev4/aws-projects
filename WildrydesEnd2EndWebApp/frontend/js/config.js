38aa23bdaa87d23ae4293bb1118ab21720907aff
johndev4 <raphmistica04@gmail.com>
2024-09-10 17:00:56 +0800

    [WildrydesEnd2EndWebApp] update config file

--- WildrydesEnd2EndWebApp/frontend/js/config.js
+++ WildrydesEnd2EndWebApp/frontend/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
   cognito: {
-    userPoolId: "ap-southeast-1_q77FCPHI8", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "7ebcdm5oisujo84tfetjd4qp8r", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-    region: "ap-southap-southeast-1", // e.g. us-east-2
+    userPoolId: "ap-southeast-1_6GC01UDzI", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "h4vengi1kaded1ukjgs5nv7a1", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-southeast-1", // e.g. us-east-2
   },
   api: {
     invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
