96d8361b057eb3e27ba26199fe0145731da318eb
johndev4 <raphmistica04@gmail.com>
2024-09-10 16:46:44 +0800

    [WildrydesEnd2EndWebApp] add cognito user pool config

--- WildrydesEnd2EndWebApp/frontend/js/config.js
+++ WildrydesEnd2EndWebApp/frontend/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "ap-southeast-1_q77FCPHI8", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "7ebcdm5oisujo84tfetjd4qp8r", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-southap-southeast-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
