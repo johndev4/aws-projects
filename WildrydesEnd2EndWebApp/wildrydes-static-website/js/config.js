4d64ea0986a0f65690c15fd0d326c1b8f53aabc3
johndev4 <raphmistica04@gmail.com>
2024-09-15 00:55:25 +0800

    [WildrydesEnd2EndWebApp] move wildrydes static website files

---
+++ WildrydesEnd2EndWebApp/wildrydes-static-website/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-1_5gP9cTKN2', 
+        userPoolClientId: '1qelqatr357sfussiueivndgh9', 
+        region: 'ap-southeast-1' 
+    },
+    api: {
+        invokeUrl: 'https://nylee8k9ma.execute-api.ap-southeast-1.amazonaws.com/Dev',
+    }
+};
