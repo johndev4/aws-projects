d0e45c425176263258f08bec4a7a1a87bc9b5b6c
johndev4 <raphmistica04@gmail.com>
2024-09-29 00:59:47 +0800

    [WildrydesEnd2EndWebApp] update app route

--- WildrydesEnd2EndWebApp/wildrydes-app/src/app/app.routes.ts
+++ WildrydesEnd2EndWebApp/wildrydes-app/src/app/app.routes.ts
@@ -5,13 +5,17 @@ export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    loadComponent: () =>
-      import('./pages/home/home.component').then((c) => c.HomeComponent),
-    canActivate: [publicHomeRedirectGuard],
+    redirectTo: 'wildrydes',
   },
   {
     path: 'app',
     loadChildren: () =>
       import('./wildrydes/wildrydes.module').then((m) => m.WildrydesModule),
+  },
+  {
+    path: 'wildrydes',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((c) => c.HomeComponent),
+    canActivate: [publicHomeRedirectGuard],
   },
 ];
